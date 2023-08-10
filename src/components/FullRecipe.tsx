c280b0b0bdf6c03c28ad03155dafe14dc251a560
dylan <dylangthomas1@gmail.com>
2023-08-09 18:42:21 -0600

    getting close

--- src/components/FullRecipe.tsx
+++ src/components/FullRecipe.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { ingredientsType } from "../App";
 
 export default function FullRecipe() {
     const location = useLocation()
@@ -10,20 +11,54 @@ export default function FullRecipe() {
         navigate(-1)
     }
 
-    function upperCase() {
-        const theWord = location.state.recipeName
+    function upperCase(word:string) {
         const newWord = []
-        for(let i = 0; i < theWord.length; i++) {
-            if(i === 0) newWord.push(theWord[0].toUpperCase())
-            else newWord.push(theWord[i])
+        for(let i = 0; i < word.length; i++) {
+            if(i === 0) newWord.push(word[0].toUpperCase())
+            else newWord.push(word[i])
         }
         return newWord
     }
 
+    const displayIngredients = location.state.ingredients.map((x: ingredientsType) => {
+        return (
+            <ul className="ingredientsList">
+                <li className="center">{upperCase(x.ingredient)} {x.quantity}{x.units}</li>
+            </ul>
+        )
+    })
+
+    const displayDirections = location.state.directions.map((step:string, index: number) => {
+        return (
+            <ul className="ingredientsList">
+                <li className="center"><strong>Step {index + 1}: </strong>{upperCase(step)}</li>
+            </ul>
+        )
+    })
+
+    const displayCategories = location.state.category.map((c:string) => {
+        return <p className="categoryCard">{upperCase(c)}</p>
+    })
+
     return (
         <div className="fullRecipe">
             <div className="headerBar"><button type="button" onClick={goBack} className="closebtn">Close Recipe</button></div>
-            <h2 className="center">{upperCase()}</h2>
+
+            <h2 className="center" style={{fontSize: '2em', fontWeight: 'bold'}}>{upperCase(location.state.recipeName)}</h2>
+
+            <div className="categoriesContainer">{displayCategories}</div>
+
+            <div className="container">
+                <h3 className="center">Ingredients</h3>
+                <hr style={{backgroundColor: 'black', margin: '7px 0'}}></hr>
+                <div className="ingredientsContainer">{displayIngredients}</div>
+            </div>
+
+            <div className="container">
+                <h3 className="center">Directions</h3>
+                <hr style={{backgroundColor: 'black', margin: '7px 0'}}></hr>
+                <div className="ingredientsContainer">{displayDirections}</div>
+            </div>
         </div>
     )
 }
